936b15271e0e86417d1f6c7d530c6b605f546300
Max <max.dickson2000@gmail.com>
2024-01-20 13:41:53 +1300

    moved secret key

--- weatherdetector/settings.py
+++ weatherdetector/settings.py
@@ -28,7 +28,7 @@ OPENWEATHERMAP_API_KEY = os.getenv('OPENWEATHERMAP_API_KEY')
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3*9#z77j3$st#4&e7im(7pjtlw(&5c$x9zmhu6g-l&t&gfj+p7'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
